b2e3a945eb38490baa063b1e3e5fd7e0077973f9
Jeremi Anastaziak <jeremianastaziak@gmail.com>
2016-10-02 11:55:50 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
   $(document).ready(function(){
 
+    $('#clients__block__slider').flexslider({
+        animation: "slide",
+        animation: "slide",
+        directionNav: false,
+        controlNav: true,
+        touch: false,
+        pauseOnHover: true,
+        start: function() {
+            $.waypoints('refresh');
+        }
+    });
+
+    $(".flex-control-nav a").text("").html("<i class='fa fa-circle' aria-hidden='true'></i>");
+
     $(document).on("scroll resize", function() {
 
         $("nav").toggleClass("shrink", ($(this).scrollTop() > 50) && ($(window).width() > 750));
@@ -15,7 +29,7 @@
         
     $("#arrow_down").on("click", function() {
         $('html,body').animate({
-            scrollTop: $(".scroll_destination").offset().top -80
+            scrollTop: $("#features").offset().top -80
         }, 1500);
     });
 
@@ -37,6 +51,47 @@
             window.location.hash = target;
             $(document).on("scroll", onScroll);
         });
+    });
+
+    $('.wp1').waypoint(function() {
+        $('.wp1').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    });
+    $('.wp2').waypoint(function() {
+        $('.wp2').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp3').waypoint(function() {
+        $('.wp3').addClass('animated fadeInDown');
+    }, {
+        offset: '55%'
+    });
+    $('.wp4').waypoint(function() {
+        $('.wp4').addClass('animated fadeInDown');
+    }, {
+        offset: '75%'
+    });
+    $('.wp5').waypoint(function() {
+        $('.wp5').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp6').waypoint(function() {
+        $('.wp6').addClass('animated fadeInDown');
+    }, {
+        offset: '75%'
+    });
+    $('.wp7').waypoint(function() {
+        $('.wp7').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp8').waypoint(function() {
+        $('.wp8').addClass('animated pulse');
+    }, {
+        offset: '75%'
     });
 });
 
